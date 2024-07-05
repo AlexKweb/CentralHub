1dc3235cb8fda774beae0b3c254a743c8b70b2ce
AlexKweb <120594926+AlexKweb@users.noreply.github.com>
2024-07-05 10:35:43 +0300

    0.0.5 bot

--- CodeCollection №1/ToLearn/NODEjs/Bot/index.js
+++ CodeCollection №1/ToLearn/NODEjs/Bot/index.js
@@ -1,6 +1,39 @@
+const TelegramApi = require('node-telegram-bot-api')
+const token = '7310883694:AAG6fKYcvvATDIjVZmOZkDIahHld5YgzHBQ'
+const bot = new TelegramApi(token,{polling:true})
+const Buttons = {reply_markup: JSON.stringify({inline_keyboard:[
+        [{text: 'Кнопка 1', callback_data:'Кнопка 1'},
+         {text: 'Кнопка 2', callback_data:'Кнопка 2'}],        
+        [{text: 'Кнопка 3', callback_data:'Кнопка 3'}]
+]})}
 
-const token = "7398601815:AAGyoRZGU3dpvCfH78iTlil7aws9N7sUJVo"
+let comands =['/start','/but']
 
+const start = () => {
 
+    bot.setMyCommands([
+        {command:comands[0],description:'Старт'},
+        {command:comands[0],description:'Кнопки'}
+    ])
 
+    bot.on('message', msg=>{
+        const text = msg.text;
+        const chatId =msg.chat.id
+         bot.sendMessage(chatId,"Команды:/start /but")
+        if (text === '/start') {
+            bot.sendMessage(chatId,"Привет " + msg.from.username )
+        }
+        else if(text === '/but'){
+            bot.sendMessage(chatId," Привет" + msg.from.first_name, Buttons)
+        }
+        else {bot.sendMessage(chatId,"Введи что-то другое")}       
+    })
 
+    bot.on('callback_query', msg=>{
+        const chatId =msg.message.chat.id
+        
+        bot.sendMessage(chatId,"Ты выбрал " + msg.data)    
+    })
+}
+
+start()

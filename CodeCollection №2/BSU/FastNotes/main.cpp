e2343d1d18d3126c679c9f47b23bee0a4c779e9c
AlexKweb <120594926+AlexKweb@users.noreply.github.com>
2025-10-25 22:13:52 +0300

    Add files via upload

---
+++ CodeCollection №2/BSU/FastNotes/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+
+        vector<vector<string>> strsRez;
+
+        // for(int i = 0; i < strs.size(); i++){
+        //     sort(strs[i].begin(), strs[i].end());
+        // }
+
+        int k = 0;
+
+        for(int i = 0; i < strs.size(); i++){
+            strsRez.push_back(vector<string>{strs[i]});
+            for(int j = i+1; j < strs.size(); j++){
+                string s1 = strs[i];
+                string s2 = strs[j];
+                sort(s1.begin(), s1.end());
+                sort(s2.begin(), s2.end());
+
+                if(s1 == s2){
+                    strsRez[i].push_back(strs[j]);
+                    strs.erase(strs.begin()+j);
+                }
+            }
+        }
+        sort(strsRez.begin(), strsRez.end());
+
+        return strsRez;
+    }
+};
+
+int main() {
+    vector<string> strs;
+    cout << "Enter elem:\n";
+    for (int i = 0; i < 3; i++) {
+        getline(cin, strs[i]);
+    }
+    Solution mysol;
+    mysol.groupAnagrams(strs);
+    return 0;
+}
